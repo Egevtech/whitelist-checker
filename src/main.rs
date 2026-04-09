63e5f1f8a56b6c11efad24239ffbd8bc000fd7ee
MikhKuts <105879783+MikhKuts@users.noreply.github.com>
2026-04-09 11:06:02 +0300

    Aboba

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,38 @@
-use std::time::Duration;
+use std::{error::Error, net::ToSocketAddrs, time::Duration};
 
-use ping_rs::{PingError, PingReply};
 
 
 
 fn main() {
-    match request("77.88.44.242".to_owned()) {
-        Ok(ok) => println!("Ping {} time={}ms", ok.address, ok.rtt),
-        Err(err) => println!("Ping error"),
+    let white_url = vec![
+        "yandex.com:443",
+        "vk.com:443",
+    ];
+    let not_white_url = vec![
+        "google.com:443",
+        "github.com:443"
+    ];
+
+    for url in white_url {
+        ping(url.to_string());
+        
     }
+
+    
 }
 
-fn request(address: String) -> Result<PingReply, PingError> {
-    let addr = &address.parse().unwrap();
-    let data = [1,2,3,4];  // ping data
-    let timeout = Duration::from_secs(1);
-    let options = ping_rs::PingOptions { ttl: 128, dont_fragment: true };
-    return ping_rs::send_ping(addr, timeout, &data, Some(&options));
+fn ping(url: String){
+    let addres = url.to_socket_addrs().unwrap().next().unwrap().ip();
+
+    match ping::new(addres)
+        .timeout(Duration::from_secs(2))
+        .ttl(128)
+        .send()
+    {
+        Ok(_) => println!("Ping successful with custom options!"),
+        Err(e) => eprintln!("Ping failed: {}", e),
+    }
+
 }
+
+
