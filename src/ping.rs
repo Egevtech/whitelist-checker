592930d11fb7018739b3af28335c8460480eb067
egevtech <egevtech@gmail.com>
2026-05-15 21:48:05 +0300

    Splitted project in files

---
+++ src/ping.rs
@@ -0,0 +1,44 @@
+use std::{net::ToSocketAddrs, time::Duration};
+
+pub fn ping(url: String, iteration: i32, repeats: i32) -> bool {
+    let addres = match url.to_socket_addrs() {
+        Ok(mut ok) => ok.next().unwrap().ip(),
+        Err(err) => {
+            eprintln!(
+                "[{}/{}] Неудачный пинг {} \nОшибка {}",
+                iteration + 1,
+                repeats,
+                url,
+                err.to_string()
+            );
+            return false;
+        }
+    };
+
+    match ping::new(addres)
+        .timeout(Duration::from_secs(2))
+        .ttl(128)
+        .send()
+    {
+        Ok(ok) => {
+            println!(
+                "[{}/{}] Пинг {} отклик={}мс",
+                iteration + 1,
+                repeats,
+                url,
+                ok.rtt.as_millis().to_string()
+            );
+            return true;
+        }
+        Err(e) => {
+            eprintln!(
+                "[{}/{}] Неудачный пинг {} \nОшибка {}",
+                iteration + 1,
+                repeats,
+                url,
+                e
+            );
+            return false;
+        }
+    };
+}
