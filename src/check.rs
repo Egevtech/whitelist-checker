690046cd4b137109b7cd5b8c52cc09974f22103c
egevtech <egevtech@gmail.com>
2026-05-16 16:33:33 +0300

    Fix typo

--- src/check.rs
+++ src/check.rs
@@ -2,7 +2,7 @@ use ping;
 use std::{net::ToSocketAddrs, time::Duration};
 
 pub fn check(url: String, iteration: i32, repeats: i32) -> bool {
-    let addres = match url.to_socket_addrs() {
+    let address = match url.to_socket_addrs() {
         Ok(mut ok) => ok.next().unwrap().ip(),
         Err(err) => {
             eprintln!(
@@ -14,7 +14,7 @@ pub fn check(url: String, iteration: i32, repeats: i32) -> bool {
         }
     };
 
-    match ping::new(addres)
+    match ping::new(address)
         .timeout(Duration::from_secs(2))
         .ttl(128)
         .send()
